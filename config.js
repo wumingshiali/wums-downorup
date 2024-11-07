2300116d5e5edabb11aec8d8d725681af6fae0b2
wumingshiali <157328007+wumingshiali@users.noreply.github.com>
2024-11-07 21:23:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,11 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur2711318-44c726a0dad5517ece7b07b3',
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 45,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -21,16 +18,8 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: '个人主页',
+      url: 'https://wums.us.kg/'
     },
   ],
 };
